2354993a74885f40bc30647edbb6a8524eacd6bf
Hunter Egnatis <hunteregnatis@gmail.com>
2024-05-11 19:41:42 -0400

    Added service for postgres. Added standalone lint

--- dagger/main.go
+++ dagger/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 )
 
 type PluginCi struct{}
 
-var php = []string{"7.4", "8.0", "8.1"}
-var moodle_version = []string{"MOODLE_401_STABLE"}
+var php = []string{"7.4", "8.0", "8.1", "8.2"}
+var moodle_version = []string{"MOODLE_401_STABLE", "MOODLE_404_STABLE"}
 var database = []string{"pgsql", "mariadb"}
 
 func (m *PluginCi) MariadbService() *Service {
@@ -22,28 +23,30 @@ func (m *PluginCi) MariadbService() *Service {
 		AsService()
 }
 
-// Init function for setting up template that other jobs draw from
-func (m *PluginCi) Init(ctx context.Context, source *Directory, dependencies *Directory) *Container {
-	// postgres := dag.Container().
-	// 	From("postgres:13").
-	// 	WithEnvVariable("POSTGRES_USER", "postgres").
-	// 	WithEnvVariable("POSTGRES_HOST_AUTH_METHOD", "trust").
-	// 	WithExposedPort(5432).
-	// 	AsService()
-
-	// Setup moodle
+func (m *PluginCi) PostgresService() *Service {
 	return dag.Container().
-		From("php:8.1-apache-bookworm").
+		From("postgres:13").
+		WithEnvVariable("POSTGRES_USER", "postgres").
+		WithEnvVariable("POSTGRES_HOST_AUTH_METHOD", "trust").
+		WithExposedPort(5432).
+		AsService()
+}
+
+// Init function for setting up template that other jobs draw from
+func (m *PluginCi) Moodle(ctx context.Context, plugin *Directory, dependencies *Directory, phpVersion string, moodleVersion string, database string) *Container {
+
+	moodle := dag.Container().
+		From(fmt.Sprintf("php:%s-apache-bullseye", phpVersion)).
 		//WithServiceBinding("pgsql", postgres).
 		WithExec([]string{"echo", "max_input_vars=5000", ">>", "/usr/local/etc/php/php.ini-production"}).
 		WithExec([]string{"apt-get", "update"}).
-		WithExec([]string{"apt-get", "install", "--yes", "git-core", "zip", "curl", "mariadb-client", "libpng-dev", "zlib1g-dev", "libicu-dev", "libzip-dev", "libxml2-dev"}).
-		WithExec([]string{"docker-php-ext-install", "pdo", "pdo_mysql", "mysqli", "gd", "intl", "zip", "soap"}).
+		WithExec([]string{"apt-get", "install", "--yes", "git-core", "zip", "curl", "mariadb-client", "libpng-dev", "zlib1g-dev", "libicu-dev", "postgresql-client", "libzip-dev", "libxml2-dev", "libpq-dev"}).
+		WithExec([]string{"docker-php-ext-install", "pdo", "pdo_mysql", "mysqli", "gd", "intl", "zip", "soap", "pgsql"}).
 		WithExec([]string{"sh", "-c", "curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer"}).
 		WithExec([]string{"curl", "-fsSL", "https://deb.nodesource.com/setup_20.x", "-o", "install.sh"}).
 		WithExec([]string{"bash", "install.sh"}).
 		WithExec([]string{"apt-get", "install", "-y", "nodejs"}).
-		WithDirectory("/var/www/html", source.WithoutDirectory("dagger"), ContainerWithDirectoryOpts{
+		WithDirectory("/var/www/html", plugin.WithoutDirectory("dagger"), ContainerWithDirectoryOpts{
 			Owner: "www-data:www-data",
 		}).
 		WithDirectory("/var/www/dependencies", dependencies.WithoutDirectory("dagger"), ContainerWithDirectoryOpts{
@@ -51,48 +54,56 @@ func (m *PluginCi) Init(ctx context.Context, source *Directory, dependencies *Di
 		}).
 		WithWorkdir("/var/www/html").
 		WithExec([]string{"chmod", "-R", "775", "/var/www"}).
-		WithEnvVariable("MOODLE_BRANCH", moodle_version[0]).
+		WithEnvVariable("MOODLE_BRANCH", moodleVersion).
 		WithEnvVariable("COMPOSER_ALLOW_SUPERUSER", "1").
-		WithEnvVariable("DB", database[1]).
-		WithExec([]string{"composer", "create-project", "-n", "--no-dev", "--prefer-dist", "moodlehq/moodle-plugin-ci", "../ci", "^4"}).
-		WithServiceBinding("db", m.MariadbService()).
-		WithNewFile("/usr/local/etc/php/conf.d/docker-php-ext-additional.ini", ContainerWithNewFileOpts{
+		WithEnvVariable("DB", database).
+		WithExec([]string{"composer", "create-project", "-n", "--no-dev", "--prefer-dist", "moodlehq/moodle-plugin-ci", "../ci", "^4"})
+
+	if database == "pgsql" {
+		return moodle.WithServiceBinding(
+			"db",
+			m.PostgresService(),
+		).
+			WithNewFile("/usr/local/etc/php/conf.d/docker-php-ext-additional.ini", ContainerWithNewFileOpts{
+				Contents: "max_input_vars = 5000",
+			}).
+			WithExec([]string{"../ci/bin/moodle-plugin-ci", "install", "--plugin", "./", "--extra-plugins", "/var/www/dependencies", "--db-host=db", "--no-init"}).
+			WithMountedCache("/root/.composer", dag.CacheVolume("composer-cache")).
+			WithMountedCache("/var/www/vendor", dag.CacheVolume("composer-vendor-cache"))
+	} else if database == "mariadb" {
+		return moodle.WithServiceBinding(
+			"db",
+			m.MariadbService(),
+		).
+			WithNewFile("/usr/local/etc/php/conf.d/docker-php-ext-additional.ini", ContainerWithNewFileOpts{
+				Contents: "max_input_vars = 5000",
+			}).
+			WithExec([]string{"../ci/bin/moodle-plugin-ci", "install", "--plugin", "./", "--extra-plugins", "/var/www/dependencies", "--db-host=db", "--no-init"}).
+			WithMountedCache("/root/.composer", dag.CacheVolume("composer-cache")).
+			WithMountedCache("/var/www/vendor", dag.CacheVolume("composer-vendor-cache"))
+	} else {
+		return moodle.WithNewFile("/usr/local/etc/php/conf.d/docker-php-ext-additional.ini", ContainerWithNewFileOpts{
 			Contents: "max_input_vars = 5000",
 		}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "install", "--plugin", "./", "--extra-plugins", "/var/www/dependencies", "--db-host=db", "--no-init"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "phplint"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "phpmd"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "phpcs"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "phpdoc"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "validate"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "savepoints"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "mustache"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "grunt"}).
-		WithExec([]string{"../ci/bin/moodle-plugin-ci", "phpunit"}).
-		WithMountedCache("/root/.composer", dag.CacheVolume("composer-cache")).
-		WithMountedCache("/var/www/vendor", dag.CacheVolume("composer-vendor-cache"))
+			WithExec([]string{"../ci/bin/moodle-plugin-ci", "install", "--plugin", "./", "--extra-plugins", "/var/www/dependencies", "--db-host=db", "--no-init"}).
+			WithMountedCache("/root/.composer", dag.CacheVolume("composer-cache")).
+			WithMountedCache("/var/www/vendor", dag.CacheVolume("composer-vendor-cache"))
+	}
+
 }
 
-// Returns a container that echoes whatever string argument is provided
-func (m *PluginCi) ContainerEcho(stringArg string) *Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+// run Phplint
+func (m *PluginCi) PhpLint(ctx context.Context, plugin *Directory, dependencies *Directory) (string, error) {
+	return m.Moodle(ctx, plugin, dependencies, "8.1", "MOODLE_401_STABLE", "pgsql").
+		WithExec([]string{"../ci/bin/moodle-plugin-ci", "phplint"}).Stdout(ctx)
 }
 
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *PluginCi) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
-}
-
-// sends a query to a MariaDB service received as input and returns the response
-func (m *PluginCi) UserList(ctx context.Context, svc *Service) (string, error) {
-	return dag.Container().
-		From("mariadb:10.11.2").
-		WithServiceBinding("db", svc).
-		WithExec([]string{"/usr/bin/mysql", "--user=root", "--password=secret", "--host=db", "-e", "SELECT Host, User FROM mysql.user"}).
-		Stdout(ctx)
-}
+//
+// WithExec([]string{"../ci/bin/moodle-plugin-ci", "phpmd"}).
+// WithExec([]string{"../ci/bin/moodle-plugin-ci", "phpcs"}).
+// WithExec([]string{"../ci/bin/moodle-plugin-ci", "phpdoc"}).
+// WithExec([]string{"../ci/bin/moodle-plugin-ci", "validate"}).
+// WithExec([]string{"../ci/bin/moodle-plugin-ci", "savepoints"}).
+// WithExec([]string{"../ci/bin/moodle-plugin-ci", "mustache"}).
+// WithExec([]string{"../ci/bin/moodle-plugin-ci", "grunt"}).
+// WithExec([]string{"../ci/bin/moodle-plugin-ci", "phpunit"}).
